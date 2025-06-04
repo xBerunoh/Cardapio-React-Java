07e3f937f5ed8991e1af9629b27274e2fa553e0d
Bruno Eduardo de Carvalho Lucena <57199262+xBerunoh@users.noreply.github.com>
2025-06-03 23:35:44 -0400

    Construtor padrão na classe food.

--- src/main/java/com/example/cardapio/food/Food.java
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -24,4 +24,10 @@ public class Food {
 
     private Integer price;
 
+    public Food(FoodRequestDTO data){
+        this.image = data.image();
+        this.price = data.price();
+        this.title = data.title();
+    }
+
 }
