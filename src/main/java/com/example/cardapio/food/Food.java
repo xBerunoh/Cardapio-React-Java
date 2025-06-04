e19ad75e9cf495c51d64f7eec6895fe6a33019cb
Bruno Eduardo de Carvalho Lucena <57199262+xBerunoh@users.noreply.github.com>
2025-06-03 22:58:03 -0400

    Adicionando o @Id e @GeneratedValue , para criar a chave primaria e gerar seu valor automaticamente.
    E a utilização da biblioteca Lombok, para gerar automatiamente os getters , para geração de constructor.

--- src/main/java/com/example/cardapio/food/Food.java
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -2,18 +2,25 @@ package com.example.cardapio.food;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.IdGeneratorType;
 
 @Table( name= "foods")
 @Entity(name = "foods")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Food {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
 
-    private String img;
+    private String image;
 
     private Integer price;
 
